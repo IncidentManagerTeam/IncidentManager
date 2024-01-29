87ad75df216a716924c5c3e8ef21fdcdf10915ae
daluga0503 <daniel.luquegallardo@gmail.com>
2024-01-29 11:21:34 +0100

    clase del lunes 29. Implementando security.

--- src/main/java/com/example/incidentmanager/User/security/adapter/SecurityController.java
+++ src/main/java/com/example/incidentmanager/User/security/adapter/SecurityController.java
@@ -1,10 +1,13 @@
 package com.example.incidentmanager.User.security.adapter;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfToken;
 
 import com.example.incidentmanager.User.domain.UserEntity;
 import com.example.incidentmanager.User.service.UserService;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -13,19 +16,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class SecurityController {
 
     private final UserService service;
+    private final PasswordEncoder encoder;
 
-    public SecurityController(UserService service){
+    public SecurityController(UserService service, PasswordEncoder encoder){
         this.service = service;
+        this.encoder = encoder;
     }
     
     @PostMapping("/register")
     public UserEntity registerUser(@RequestBody UserEntity entity) {
+        String encodedPassword = this.encoder.encode(entity.getPassword());
+        entity.setPassword(encodedPassword);
         return service.create(entity);
     }
 
     @GetMapping("/csrf")
     public CsrfToken getCsrfToken(CsrfToken token){
-        return token
+        return token;
     }
     
 }
